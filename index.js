234650e9b3eeded4b7124ce11a4a55e9253b1b4a
Justin James <justinmjames@protonmail.com>
2022-05-29 17:59:05 -0700

    commit tree 5e830df7967e88b7749ccd7f1a5804e40bb50d05

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(arr, driverName){
+    let matchDriver = arr.filter(element => element.toLowerCase() === driverName.toLowerCase());
+    return matchDriver;
+    };
+   
+
+function fuzzyMatch(arr, query){
+    let letters = arr.filter(element => element.startsWith(query))
+    return letters;
+}
+
+
+function matchName(drivers, name){
+    return drivers.filter(element => element.name.toLowerCase() === name.toLowerCase())
+};
+    
